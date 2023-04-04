d92756d1bbf905f5d2658c512234447e4e0b78ee
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-04 23:20:04 +0200

    chore: release v0.3.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Refined GitHub Notifications
 // @namespace    https://greasyfork.org/en/scripts/461320-refined-github-notifications
-// @version      0.2.8
+// @version      0.3.0
 // @description  Enhances the GitHub Notifications page, making it more productive and less noisy.
 // @author       Anthony Fu (https://github.com/antfu)
 // @license      MIT
